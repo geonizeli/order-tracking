c25507f2e928b2c2acfc8510e2f16c077fadd948
João Geonizeli <joao.geonizeli@gmail.com>
2022-09-21 19:02:30 -0300

    add action text to order description

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,6 @@ class Order < ApplicationRecord
   enumerize :status, in: [:pending, :processing, :completed, :cancelled], default: :pending
 
   validates :status, presence: true
+
+  has_rich_text :description
 end
