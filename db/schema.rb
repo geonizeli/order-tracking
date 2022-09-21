bb002490b5dfddd2ec670c0baaecc7538d89b866
João Geonizeli <joao.geonizeli@gmail.com>
2022-09-21 18:38:28 -0300

    add order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_204402) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_211728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,4 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_204402) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "status"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "orders", "customers"
 end
