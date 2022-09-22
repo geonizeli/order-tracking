d6ea8f4fd5be807a4da4b3a0fecb2da1353bb145
João Geonizeli <joao.geonizeli@gmail.com>
2022-09-22 20:28:38 -0300

    use time-ago if dates are recents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def show_time_ago_if_recent(time)
+    if time > 24.hour.ago
+      time_tag(time,  'data-local': 'time-ago')
+    else
+      time_tag(time)
+    end
+  end
 end
